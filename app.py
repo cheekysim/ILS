e24965e472ca6f642769bed85f6a919a198a0092
cheekysim <minecraftcheekysim@gmail.com>
2022-12-16 20:06:53 +0000

    Added basicauth to creds Fixed Submit Button Fixed css files

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template, jsonify, request, redirect
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from main import getData
 import json
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+with open('auth.json', "r") as f:
+    logins = json.load(f)
+    print(logins)
+
+users = {username: generate_password_hash(password) for username, password in logins.items()}
+
+@auth.verify_password
+def veify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
 
 
 @app.route('/data', methods=['GET'])
@@ -13,6 +27,7 @@ def date():
     return jsonify(data)
 
 @app.route('/creds', methods=['GET', 'POST'])
+@auth.login_required
 def creds():
     if request.method == 'POST':
         with open('creds.json', 'w') as f:
