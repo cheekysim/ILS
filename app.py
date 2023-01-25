bd7bcd96883e288c79f1a2f39fba687c02dd5633
cheeky sim <48182295+cheekysim@users.noreply.github.com>
2023-01-25 15:45:38 +0000

    updates to index.html and app.py

--- app.py
+++ app.py
@@ -1,46 +1,27 @@
 from flask import Flask, render_template, jsonify, request, redirect
-from flask_httpauth import HTTPBasicAuth
-from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from main import getData
 import json
 
 app = Flask(__name__)
-auth = HTTPBasicAuth()
-
-
-@auth.verify_password
-def veify_password(username, password):
-    global login
-    try:
-        with open('auth.json', 'r') as f:
-            login = json.load(f)
-    except FileNotFoundError:
-        with open('auth.json', 'w') as f:
-            login = {"user": "pass"}
-            json.dump(login, f, indent=4)
-
-    users = {username: generate_password_hash(password) for username, password in login.items()}
-    if username in users and check_password_hash(users.get(username), password):
-        return username
-
 
 @app.route('/data', methods=['GET'])
-def date():
+def data():
     cg, br = getData()
     data = {"cg": cg, "br": br}
     return jsonify(data)
 
+@app.route('/creds-invalid', methods=['GET'])
+def creds_invalid():
+    return render_template("creds-invalid.html")
+
 @app.route('/creds', methods=['GET', 'POST'])
-@auth.login_required
 def creds():
     if request.method == 'POST':
-        global login
-        if login == {"user": "pass"}:
-            with open('auth.json', 'w') as f:
-                json.dump({request.form.get('email'): request.form.get('password')}, f, indent=4)
-            login = {request.form.get('email'): request.form.get('password')}
-            return redirect('/creds')
+        data = getData()
+        print(data)
+        if data == ({'points': 0, 'position': 0}, {'points': 0, 'position': 0}):
+            return redirect('/creds-invalid')
         else:
             with open('creds.json', 'w') as f:
                 json.dump(request.form, f, indent=4)
